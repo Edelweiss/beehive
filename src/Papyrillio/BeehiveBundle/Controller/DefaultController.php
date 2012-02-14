74f92cc734039649f6fbef7fa3d5c3b297f2b879
Carmen Maria Lanz <carmen_lanz@ossiriand.de>
2012-02-14 08:00:30 +0100

    add skeletons for static HTML pages (about, contact, help)

--- src/Papyrillio/BeehiveBundle/Controller/DefaultController.php
+++ src/Papyrillio/BeehiveBundle/Controller/DefaultController.php
@@ -4,12 +4,17 @@ namespace Papyrillio\BeehiveBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-
-class DefaultController extends Controller
-{
-    
-    public function indexAction($name)
-    {
+class DefaultController extends Controller{
+    public function aboutAction(){
+        return $this->render('PapyrillioBeehiveBundle:Default:about.html.twig');
+    }
+    public function contactAction(){
+        return $this->render('PapyrillioBeehiveBundle:Default:contact.html.twig');
+    }
+    public function helpAction(){
+        return $this->render('PapyrillioBeehiveBundle:Default:help.html.twig');
+    }
+    public function indexAction($name){
         return $this->render('PapyrillioBeehiveBundle:Default:index.html.twig', array('name' => $name));
     }
 }

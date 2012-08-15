3665961389770fd77533a061c9a1fd34af836700
Carmen Maria Lanz (Aquila) <elemmire@ossiriand.de>
2012-08-15 12:26:28 +0200

    guard against null values in log entries

--- src/vendor/bundles/Stof/DoctrineExtensionsBundle/Entity/LogEntry.php
+++ src/vendor/bundles/Stof/DoctrineExtensionsBundle/Entity/LogEntry.php
@@ -11,10 +11,12 @@ class LogEntry extends AbstractLogEntry
   {
       $dataDecoded = array();
       foreach($this->data as $key => $value){
-        if($value instanceof DateTime){
-          $dataDecoded[$key] = $value->format('Y-m-d H:i:s');
-        } else {
+        if (is_string($value)) {
           $dataDecoded[$key] = \Papyrillio\BeehiveBundle\Entity\Correction::decode4Byte($value);
+        } else if($value instanceof DateTime){
+          $dataDecoded[$key] = $value->format('Y-m-d H:i:s');
+        } else if ($value === null) {
+          $dataDecoded[$key] = null;
         }
       }
       return $dataDecoded;

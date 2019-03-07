79f05ad344e64aeefb9259963fc446d53d533c85
Carmen Maria Lanz (Aquila) <elemmire@ossiriand.de>
2019-03-07 14:23:43 +0100

    draw ddb information from HGV number, persist information in database

--- src/Papyrillio/BeehiveBundle/DataFixtures/ORM/Import/ImportFromCsv.php
+++ src/Papyrillio/BeehiveBundle/DataFixtures/ORM/Import/ImportFromCsv.php
@@ -8,6 +8,8 @@ use Papyrillio\BeehiveBundle\Entity\Compilation;
 use Papyrillio\BeehiveBundle\Entity\Correction;
 use Papyrillio\BeehiveBundle\Entity\Task;
 use DateTime;
+use DOMDocument;
+use DOMXPath;
 
 /*
 
@@ -19,7 +21,7 @@ class ImportFromCsv extends AbstractFixture implements OrderedFixtureInterface
 {
     const CSV_COMPILATION_TITLE = 0;
     const CSV_COMPILATION_PAGE  = 1;
-    const CSV_TM                = 2;
+    const CSV_HGV               = 2;
     const CSV_TEXT1             = 3;
     const CSV_TEXT2             = 4;
     const CSV_POSITION          = 5;
@@ -29,6 +31,7 @@ class ImportFromCsv extends AbstractFixture implements OrderedFixtureInterface
     const CSV_CREATOR           = 9;
 
     const IMPORT_FILE = 'src/Papyrillio/BeehiveBundle/Resources/data/import.csv';
+    const IDNO_FILE   = 'src/Papyrillio/BeehiveBundle/Resources/data/idno.xml';
     
     const DEFAULT_STATUS = 'unchecked';
     const DEFAULT_CREATOR = 'system';
@@ -36,6 +39,8 @@ class ImportFromCsv extends AbstractFixture implements OrderedFixtureInterface
     protected $editionList = array();
     protected $compilationList = array();
     protected $editionExampleCorrection = array();
+    
+    protected static $idnoXpath = null;
 
     function load(ObjectManager $manager)
     {
@@ -47,43 +52,52 @@ class ImportFromCsv extends AbstractFixture implements OrderedFixtureInterface
          while(($data = fgetcsv($handle, 1000, ',')) !== FALSE){
            $compilationTitle = $data[self::CSV_COMPILATION_TITLE];
            $compilationPage  = $data[self::CSV_COMPILATION_PAGE];
-           $tm               = $data[self::CSV_TM];
+           $hgv              = $data[self::CSV_HGV];
+           $tm               = preg_replace('/[a-z]+/', '', $hgv);
            $folder           = ceil($tm / 1000);
-           $hgv              = $tm;
            $text1            = $data[self::CSV_TEXT1];
            $text2            = $data[self::CSV_TEXT2];
            $position         = $data[self::CSV_POSITION];
            $description      = $data[self::CSV_DESCRIPTION];
            $editionSort      = $data[self::CSV_EDITION_SORT];
-           $source           = $data[self::CSV_SOURCE];
-           $creator          = self::DEFAULT_CREATOR;
+           $source           = is_numeric($data[self::CSV_SOURCE]) ? $data[self::CSV_SOURCE] : null;
+           $creator          = $data[self::CSV_CREATOR];
            $status           = self::DEFAULT_STATUS;
 
-           $edition = $this->getEdition($editionSort, $manager);
-           $editionX = $this->getEditionExampleCorrection($editionSort, $manager);           
-           $editionId = $editionX->getEdition()->getId();
-
+           $ddb = $this->getDdb($hgv);
 
            $correction = new Correction();
-           $correction->setEdition($editionX->getEdition());
+           $correction->setEdition($this->getEdition($editionSort, $manager));
            $correction->setCompilation($this->getCompilation($compilationTitle, $manager));
            $correction->setText($this->formatText($text1, $text2, $editionSort));
-           $correction->setDdb($editionX->getDdb());
-           $correction->setCollection($editionX->getCollection());
-           $correction->setVolume($editionX->getVolume());
-           $correction->setDocument($editionX->getDocument());
+           $correction->setDdb($ddb['ddb']);
+           $correction->setCollection($ddb['collection']);
+           $correction->setVolume($ddb['volume']);
+           $correction->setDocument($ddb['document']);
+           $correction->setDescription($description);
+           $correction->setTm($tm);
+           $correction->setHgv($hgv);
+           $correction->setFolder($folder);
+           $correction->setPosition($position);
+           $correction->setSource($source);
+           $correction->setStatus($status);
+           $correction->setCreator($creator);
+           $correction->setCompilationPage($compilationPage);
            
 
            //echo $row . '> '. $compilation . '|' . $compilationPage . '|' . $tm . '|' . $text . '|' . $text2 . '|' . $position . '|' . $description . '|' . $editionSort . '|' . $source . '|' . $creator . "\n";
            echo $row . '> Edition: ' . $correction->getEdition()->getId() . ' DDB: ' . $correction->getDdb() . ' (' . $correction->getCollection() . '|' . $correction->getVolume() . '|' . $correction->getDocument() . ")\n";
            echo $row . '> Compilation: ' . $correction->getCompilation()->getId() . ' ' . $correction->getCompilation()->getTitle() . "\n";
-           echo $row . '> TM: ' . $tm . ' (' . $folder . ")\n";
+           echo $row . '> HGV/TM: ' . $hgv . '/' . $tm . ' (' . $folder . ")\n";
            echo $row . '> Text: ' . $correction->getText() . "\n";
            echo $row . "> --\n";
+
+           $manager->persist($correction);
 
            $row++;
 
        }
+       $manager->flush();
        fclose($handle);
       }
     }
@@ -91,6 +105,26 @@ class ImportFromCsv extends AbstractFixture implements OrderedFixtureInterface
     public function getOrder()
     {
         return 1;
+    }
+    
+    protected function getDdb($hgv){
+      if(!self::$idnoXpath){
+        $doc = new DOMDocument();
+        $doc->load(self::IDNO_FILE);
+        self::$idnoXpath = new DOMXPath($doc);
+      }
+
+      //$xpath->registerNamespace('fm', self::NAMESPACE_FILEMAKER);
+      
+      $ddb = ';;';
+      $ddbIdno = self::$idnoXpath->evaluate("/list/item[idno[@type='hgv'][string(.)='" . $hgv . "']]/idno[@type='ddb']");
+
+      if($ddbIdno->length > 0){
+        $ddb = $ddbIdno->item(0)->nodeValue;
+      }
+
+      $ddbExploded = explode(';', $ddb);
+      return array('ddb' => $ddb, 'collection' => $ddbExploded[0], 'volume' => $ddbExploded[1], 'document' => $ddbExploded[2]);
     }
 
     protected function formatText($text1, $text2, $editionSort){

cf16c2fdac5879ba2301a73250f69f405494686d
Carmen Maria Lanz (Aquila) <elemmire@ossiriand.de>
2019-09-24 14:25:43 +0200

    set hgv before setting tm, because setting hgv will always overwrite the content of tm accordingly (which in 99% is a useful feature... but...)

--- src/Papyrillio/BeehiveBundle/DataFixtures/ORM/UpdateRegister/UpdateRegister.php
+++ src/Papyrillio/BeehiveBundle/DataFixtures/ORM/UpdateRegister/UpdateRegister.php
@@ -49,6 +49,7 @@ class UpdateRegister extends AbstractFixture implements OrderedFixtureInterface
 
     protected function updateRegisterFromEpiDocIdnos($manager){
       $itemList = self::getIdnoXpath()->evaluate("/list/item[not(idno[@type='hgv']) or number(idno[@type='hgv']) < 500000 or number(idno[@type='hgv']) > 501000]");
+      //$itemList = self::getIdnoXpath()->evaluate("/list/item[idno[@type='hgv'] and (number(idno[@type='hgv']) < 500000 or number(idno[@type='hgv']) > 501000)]");
       for($i = 0; $i < $itemList->length; $i++){
         // get idnos from EpiDoc
         $hgvIdno  = self::getIdnoXpath()->query("idno[@type='hgv']", $itemList->item($i));
@@ -103,8 +104,8 @@ class UpdateRegister extends AbstractFixture implements OrderedFixtureInterface
           }
         } elseif(count($matchingRegisterEntry) === 0){ // NEW
             $register = new Register();
-            $register->setTm($tmIdno);
             $register->setHgv($hgvIdno);
+            $register->setTm($tmIdno);
             $register->setDdb($ddbIdno);
             $register->setDclp($dclpIdno);
             $manager->persist($register);

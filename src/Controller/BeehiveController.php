36b910839c586735f28ed6881c87a4af120c84c9
Elor Oké (Piccolo) <elor.oke@adamsakte.de>
2021-06-16 23:57:28 +0200

    add logger

--- src/Controller/BeehiveController.php
+++ src/Controller/BeehiveController.php
@@ -7,17 +7,20 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\RequestStack;
+use Psr\Log\LoggerInterface;
 use App\Entity\Task;
 use App\Entity\IndexEntry;
 
 class BeehiveController extends AbstractController{
   protected $request;
   protected $allParameters = [];
+  protected $logger;
 
-  public function __construct(RequestStack $requestStack)
+  public function __construct(RequestStack $requestStack, LoggerInterface $logger)
   {
       $this->request = $requestStack->getCurrentRequest();
       $this->allParameters = array_merge($this->request->request->all(), $this->request->query->all());
+      $this->logger = $logger;
   }
 
   protected function getParameter($key){

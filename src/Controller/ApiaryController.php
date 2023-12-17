c871db48f7e2a1a84e73ada8e642b64c51f79664
Elor Oké (Piccolo) <elor.oke@adamsakte.de>
2023-12-18 00:21:12 +0100

    new frontend

--- src/Controller/ApiaryController.php
+++ src/Controller/ApiaryController.php
@@ -12,10 +12,28 @@ use DateTime;
 
 class ApiaryController extends BeehiveController{
 
-  protected static $TYPES = array('tm' => 'r.tm', 'hgv' => 'r.hgv', 'ddb' => 'r.ddb', 'biblio' => 'c.source', 'bl' => 'c2.volume', 'register' => 'r.id', 'boep' => 'c2.title', 'volume' => 'r.ddb');
+  protected static $TYPES = array('tm' => 'r.tm', 'hgv' => 'r.hgv', 'ddb' => 'r.ddb', 'biblio' => 'c.source', 'bl' => 'c2.volume', 'register' => 'r.id', 'boep' => 'c2.title', 'collection' => 'c2.collection', 'volume' => 'r.ddb');
 
   public function index(): Response{
     return $this->render('apiary/index.html.twig');
+  }
+
+  private function getCompilations($type, $id){
+    $entityManager = $this->getDoctrine()->getManager();
+    $repository = $entityManager->getRepository(Compilation::class);
+    $result = [];
+    if($type === 'collection'){
+      $result = $repository->findBy(['collection' => $id], ['collection' => 'DESC', 'volume' => 'ASC']);
+    } elseif ($type === 'BL') {
+      $result = $repository->findBy(['collection' => 'BL'], ['collection' => 'DESC', 'volume' => 'ASC']);
+    } elseif ($type === 'BOEP') {
+      $result = $repository->findBy(['collection' => 'BOEP'], ['collection' => 'DESC', 'volume' => 'ASC']);
+    } elseif ($type === 'BL Konk.') {
+      $result = $repository->findBy(['collection' => 'BL Konk.'], ['collection' => 'DESC', 'volume' => 'ASC']);
+    } else {
+      $result = $repository->findBy([], ['volume' => 'ASC']);
+    }
+    return $result;
   }
 
   public function honey($type, $id, $format = 'html'): Response{
@@ -39,24 +57,33 @@ class ApiaryController extends BeehiveController{
       $parameters['status'] = 'finalised';
       $parameters['compilationTitle'] = 'XII';
     }
+  
+    // SORT
+    $sort = 'c.sort';
+    if(($type === 'collection' && $id === 'BOEP') || ($type === 'boep' && $id === 'Bulletin of Online Emendations to Papyri')){
+      $sort = 'c.sort';
+    } elseif ($type === 'boep' && preg_match('/Bulletin of Online Emendations to Papyri [\d.]+/', $id)) {
+      $sort = 'c.compilationIndex';
+    }
 
     // QUERY
 
     $query = $entityManager->createQuery('
       SELECT e, c, t FROM App\Entity\Correction c
-      LEFT JOIN c.registerEntries r LEFT JOIN c.tasks t JOIN c.edition e JOIN c.compilation c2 WHERE ' . $where . ' ORDER BY c.sort'
+      LEFT JOIN c.registerEntries r LEFT JOIN c.tasks t JOIN c.edition e JOIN c.compilation c2 WHERE ' . $where . ' ORDER BY ' . $sort
     );
 
     $query->setParameters($parameters);
 
     $corrections = $query->getResult();
+    $compilations = $this->getCompilations($type, $id);
 
     if($format === 'html'){
-      return $this->render('apiary/honey.html.twig', ['corrections' => $corrections]);
+      return $this->render('apiary/honey.html.twig', ['corrections' => $corrections, 'compilations' => $compilations]);
     } elseif($format === 'plain'){
-      return $this->render('apiary/snippetHoney.html.twig', ['corrections' => $corrections]);
+      return $this->render('apiary/snippetHoney.html.twig', ['corrections' => $corrections, 'compilations' => $compilations]);
     } elseif ($format === 'rdf') {
-      $response = new Response($this->renderView('apiary/honey.xml.twig', array('corrections' => $corrections)));
+      $response = new Response($this->renderView('apiary/honey.xml.twig', ['corrections' => $corrections, 'compilations' => $compilations]));
       $response->headers->set('Content-Type', 'application/rdf+xml'); //$response->headers->set('Content-Type', 'text/xml');
       return $response;
     } else {
--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,8 +2,17 @@
 
 namespace App\Controller;
 use Symfony\Component\HttpFoundation\Response;
+use App\Entity\Compilation;
 
 class DefaultController extends BeehiveController{
+    public function archive(): Response {
+        $entityManager = $this->getDoctrine()->getManager();
+        $repository = $entityManager->getRepository(Compilation::class);
+        $bl = $repository->findBy(['collection' => 'BL'], ['volume' => 'ASC']);
+        $konk = $repository->findBy(['collection' => 'BL Konk.'], ['volume' => 'ASC']);
+        $boep = $repository->findBy(['collection' => 'BOEP'], ['volume' => 'ASC']);
+        return $this->render('default/archive.html.twig', ['bl' => $bl, 'konk' => $konk, 'boep' => $boep]);
+    }
     public function about(): Response {
         return $this->render('default/about.html.twig');
     }

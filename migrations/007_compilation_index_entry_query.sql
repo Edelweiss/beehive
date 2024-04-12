f634d33587b35f769b2a6bb510e5dc75a6f612c8
Elor Oké (Piccolo) <elor.oke@adamsakte.de>
2024-04-13 00:17:17 +0200

    query index_entry

--- migrations/007_compilation_index_entry_query.sql
+++ migrations/007_compilation_index_entry_query.sql
@@ -1,9 +1,10 @@
 SELECT
 
-c.short, type, topic, tab, CONCAT(IF(papy_new = 1, '*', ''), IF(greek_new = 1, '✝', ''), phrase) AS phrase, CONCAT('(', i.id, ')') as index_entry_id
+c.short, type, topic, tab, CONCAT(IF(papy_new = 1, '*', ''), IF(greek_new = 1, '✝', ''), phrase) AS phrase, i.id as index_entry_id, i.sort
 
 FROM compilation c JOIN `compilation_index_entry` ci ON ci.compilation_id = c.id JOIN index_entry i ON ci.index_entry_id = i.id
 
-WHERE ci.compilation_id = 8
+WHERE ci.compilation_id = 12
 
-ORDER BY i.sort;
+ORDER BY i.sort
+

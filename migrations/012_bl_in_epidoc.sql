5e93f4f8311d88ee6e3500afb3ba1f97f191b144
Elor Oké (Piccolo) <elor.oke@adamsakte.de>
2025-02-26 10:06:48 +0100

    sql for bl in EpiDoc

--- migrations/012_bl_in_epidoc.sql
+++ migrations/012_bl_in_epidoc.sql
@@ -2,7 +2,7 @@ SELECT hgv, GROUP_CONCAT(short, ' ', page SEPARATOR '; ') AS bl, SUM(entries) as
 
 (SELECT hgv, short, GROUP_CONCAT(compilationPage SEPARATOR ', ') AS page, COUNT(*) AS entries FROM 
 
-(SELECT hgv, short, compilationPage from correction c JOIN correction_register cr ON cr.correction_id = c.id JOIN register r ON cr.register_id = r.id JOIN compilation comp ON c.compilation_id = comp.id WHERE hgv IS NOT NULL AND comp.collection = 'BL' GROUP BY hgv, short, compilationPage ORDER BY hgv, comp.volume, c.compilationPage+0) AS s
+(SELECT hgv, short, compilationPage, IF(compilationPage, CONCAT('<item hgv="', hgv, '" bl="', short, '" page="', compilationPage, '"/>'), CONCAT('<item hgv="', hgv, '" bl="', short, '"/>')) AS epidoc from correction c JOIN correction_register cr ON cr.correction_id = c.id JOIN register r ON cr.register_id = r.id JOIN compilation comp ON c.compilation_id = comp.id WHERE hgv IS NOT NULL AND comp.collection = 'BL' GROUP BY hgv, short, compilationPage ORDER BY hgv, comp.volume, c.compilationPage+0) AS s
 
 GROUP BY hgv, short) AS S2
 
